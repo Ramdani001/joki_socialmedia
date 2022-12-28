d482c1a9ec6df5ada7de7c9d27aef703fc4ea282
unknown <rizkan0990@gmail.com>
2022-12-28 14:25:50 +0700

    100% belum revisi

---
+++ style/script.js
@@ -0,0 +1,10 @@
+
+function hideSearch() {
+    document.getElementById('hideSearch').classList.remove("d-none");
+    document.getElementById('showSearch').classList.add("d-none");
+}
+
+function showSearch() {
+    document.getElementById('hideSearch').classList.add("d-none");
+    document.getElementById('showSearch').classList.remove("d-none");
+}
